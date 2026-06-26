c7eb50cb7ef9e9bff6c9c1582e307b5b8de8209b
mangalpal <mangalpalsingh4@gmail.com>
2026-06-26 09:56:45 +0530

    solved error in process

--- calculater.py
+++ calculater.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-def calculator(num1,num2,operator):
+def calculate(num1,num2,operator):
     if operator =="+":
         return num1 + num2
     if operator =="-":
@@ -16,6 +16,6 @@ st.title("calculator")
 num1= st.number_input("enter the first number")
 num2= st.number_input("enter the second number")
 operator= st.selectbox("select the operator",["+","-","/","*"])
-result = calculator(num1,num2,operator)
+result = calculate(num1,num2,operator)
 
 st.write("the result is:",result)
