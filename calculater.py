12a6296d03264c3c1575acc366cd8f00f6f659d7
mangalpal <mangapalsingh4@gmail.com>
2026-06-29 09:55:17 +0530

    modified caclulator

--- calculater.py
+++ calculater.py
@@ -3,21 +3,27 @@ import streamlit as st
 def calculate(num1,num2,operator):
     if operator =="+":
         return num1 + num2
-    if operator =="-":
+    elif operator =="-":
         return num1 - num2
-    if operator =="*":
+    elif operator =="*":
         if num2==0:
             return "cannot divide by zero"
         return num1 * num2
-    if operator =="/":
+    elif operator =="/":
         return num1 / num2
+    elif operator =="**":
+        return num1 ** num2
+    elif operator =="//":
+        return num1 // num2
+    elif operator =="%":
+        return num1 % num2
     else:
         return "invalid operator"
 
 st.title("calculator")
 num1= st.number_input("enter the first number")
 num2= st.number_input("enter the second number")
-operator= st.selectbox("select the operator",["+","-","/","*"])
+operator= st.selectbox("select the operator",["+","-","/","*","%","**","//"])
 if st.button("calculate"):
     result = calculate(num1,num2,operator)
 
